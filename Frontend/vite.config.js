5fa97c07e73bf3c3130042bf83941bfc8610a827
Yashasvi2k3 <114045799+Yashasvi2k3@users.noreply.github.com>
2025-09-02 21:29:15 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:3000',
+        target:'https://nexus-zyou.onrender.com',
         secure:false
       }
     }
