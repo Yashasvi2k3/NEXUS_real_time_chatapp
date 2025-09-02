142d3977a81307617094102c3acb8612e5f7d2a7
Yashasvi2k3 <114045799+Yashasvi2k3@users.noreply.github.com>
2025-09-02 20:45:22 +0530

    Update jwtwebToken.js

--- Backend/utils/jwtwebToken.js
+++ Backend/utils/jwtwebToken.js
@@ -8,7 +8,8 @@ const jwtToken = (userId , res)=>{
     res.cookie('jwt', token, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        // Cross-site cookie required when frontend and backend are on different domains
+        sameSite: "none",
         secure: true
     });
 
