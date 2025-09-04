3a29c9a82caa57394e627e5f7e2829fec911b32c
Yashasvi2k3 <114045799+Yashasvi2k3@users.noreply.github.com>
2025-09-04 23:53:43 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// "https://nexus-zyou.onrender.com"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
       '/api':{
-        target:'https://nexus-zyou.onrender.com',
+        target:"http://localhost:3000/",
         secure:false
       }
     }
